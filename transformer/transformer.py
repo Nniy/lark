bb8902eb4d217608b941eaf72c863b79544766b7
Zongyu <alexyin1012@gmail.com>
2019-02-04 17:14:37 +0000

    Transformer update.
    Signed-off-by: Zongyu <alexyin1012@gmail.com>

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -2,7 +2,9 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import math, copy, time
+import math
+import copy
+import time
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
 import seaborn
