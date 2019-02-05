7e89b6f4d3d537dd409e8da2f14398c6e3113c52
Nniy <alexyin1012@gmail.com>
2019-02-05 23:45:17 +0000

    Transformer update.
    Signed-off-by: Nniy <alexyin1012@gmail.com>

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -435,70 +435,295 @@ def loss(x):
 # plt.show()
 
 
-# First example
+# # First example
+#
+# def data_gen(V, batch, nbatches):
+#     "Generate random data for a src-tgt copy task."
+#     for i in range(nbatches):
+#         data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+#         data[:, 0] = 1
+#         src = Variable(data, requires_grad=False)
+#         tgt = Variable(data, requires_grad=False)
+#         yield Batch(src, tgt, 0)
+#
+#
+# class SimpleLossCompute:
+#     "A simple loss compute and train function."
+#
+#     def __init__(self, generator, criterion, opt=None):
+#         self.generator = generator
+#         self.criterion = criterion
+#         self.opt = opt
+#
+#     def __call__(self, x, y, norm):
+#         x = self.generator(x)
+#         loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+#                               y.contiguous().view(-1)) / norm
+#         loss.backward()
+#         if self.opt is not None:
+#             self.opt.step()
+#             self.opt.optimizer.zero_grad()
+#         return loss.item() * norm  # was .data[0]
+#
+#
+# # Train the simple copy task.
+# V = 11
+# criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+# model = make_model(V, V, N=2)
+# model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
+#                     torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+#
+# for epoch in range(10):
+#     model.train()
+#     run_epoch(data_gen(V, 30, 20), model,
+#               SimpleLossCompute(model.generator, criterion, model_opt))
+#     model.eval()
+#     print(run_epoch(data_gen(V, 30, 5), model,
+#                     SimpleLossCompute(model.generator, criterion, None)))
+#
+#
+# def greedy_decode(model, src, src_mask, max_len, start_symbol):
+#     memory = model.encode(src, src_mask)
+#     ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+#     for i in range(max_len - 1):
+#         out = model.decode(memory, src_mask,
+#                            Variable(ys),
+#                            Variable(subsequent_mask(ys.size(1))
+#                                     .type_as(src.data)))
+#         prob = model.generator(out[:, -1])
+#         _, next_word = torch.max(prob, dim=1)
+#         next_word = next_word.item()  # was .data[0]
+#         ys = torch.cat([ys,
+#                         torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+#     return ys
+#
+#
+# model.eval()
+# src = Variable(torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]))
+# src_mask = Variable(torch.ones(1, 1, 10))
+# print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
 
-def data_gen(V, batch, nbatches):
-    "Generate random data for a src-tgt copy task."
-    for i in range(nbatches):
-        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
-        data[:, 0] = 1
-        src = Variable(data, requires_grad=False)
-        tgt = Variable(data, requires_grad=False)
-        yield Batch(src, tgt, 0)
+# For data loading.
+from torchtext import data, datasets
+
+if True:
+    import spacy
+
+    spacy_de = spacy.load('de')
+    spacy_en = spacy.load('en')
 
 
-class SimpleLossCompute:
-    "A simple loss compute and train function."
+    def tokenize_de(text):
+        return [tok.text for tok in spacy_de.tokenizer(text)]
 
-    def __init__(self, generator, criterion, opt=None):
+
+    def tokenize_en(text):
+        return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+    BOS_WORD = '<s>'
+    EOS_WORD = '</s>'
+    BLANK_WORD = "<blank>"
+    SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+    TGT = data.Field(tokenize=tokenize_en, init_token=BOS_WORD,
+                     eos_token=EOS_WORD, pad_token=BLANK_WORD)
+
+    MAX_LEN = 100
+    train, val, test = datasets.IWSLT.splits(
+        exts=('.de', '.en'), fields=(SRC, TGT),
+        filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+                              len(vars(x)['trg']) <= MAX_LEN)
+    MIN_FREQ = 2
+    SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+    TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+
+            self.batches = pool(self.data(), self.random_shuffler)
+
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    return Batch(src, trg, pad_idx)
+
+
+# Skip if not interested in multigpu.
+class MultiGPULossCompute:
+    "A multi-gpu loss compute and train function."
+
+    def __init__(self, generator, criterion, devices, opt=None, chunk_size=5):
+        # Send out to different gpus.
         self.generator = generator
-        self.criterion = criterion
+        self.criterion = nn.parallel.replicate(criterion,
+                                               devices=devices)
         self.opt = opt
+        self.devices = devices
+        self.chunk_size = chunk_size
 
-    def __call__(self, x, y, norm):
-        x = self.generator(x)
-        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
-                              y.contiguous().view(-1)) / norm
-        loss.backward()
+    def __call__(self, out, targets, normalize):
+        total = 0.0
+        generator = nn.parallel.replicate(self.generator,
+                                          devices=self.devices)
+        out_scatter = nn.parallel.scatter(out,
+                                          target_gpus=self.devices)
+        out_grad = [[] for _ in out_scatter]
+        targets = nn.parallel.scatter(targets,
+                                      target_gpus=self.devices)
+
+        # Divide generating into chunks.
+        chunk_size = self.chunk_size
+        for i in range(0, out_scatter[0].size(1), chunk_size):
+            # Predict distributions
+            out_column = [[Variable(o[:, i:i + chunk_size].data,
+                                    requires_grad=self.opt is not None)]
+                          for o in out_scatter]
+            gen = nn.parallel.parallel_apply(generator, out_column)
+
+            # Compute loss.
+            y = [(g.contiguous().view(-1, g.size(-1)),
+                  t[:, i:i + chunk_size].contiguous().view(-1))
+                 for g, t in zip(gen, targets)]
+            loss = nn.parallel.parallel_apply(self.criterion, y)
+
+            # Sum and normalize loss
+            l = nn.parallel.gather(loss,
+                                   target_device=self.devices[0])
+            l = l.sum()[0] / normalize
+            total += l.data[0]
+
+            # Backprop loss to output of transformer
+            if self.opt is not None:
+                l.backward()
+                for j, l in enumerate(loss):
+                    out_grad[j].append(out_column[j][0].grad.data.clone())
+
+        # Backprop all loss through transformer.
         if self.opt is not None:
+            out_grad = [Variable(torch.cat(og, dim=1)) for og in out_grad]
+            o1 = out
+            o2 = nn.parallel.gather(out_grad,
+                                    target_device=self.devices[0])
+            o1.backward(gradient=o2)
             self.opt.step()
             self.opt.optimizer.zero_grad()
-        return loss.item() * norm  # was .data[0]
+        return total * normalize
 
 
-# Train the simple copy task.
-V = 11
-criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
-model = make_model(V, V, N=2)
-model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
-                    torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+# GPUs to use
+devices = [0]  # [0, 1, 2, 3]
+if True:
+    pad_idx = TGT.vocab.stoi["<blank>"]
+    model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
+    model.cuda()
+    criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
+    criterion.cuda()
+    BATCH_SIZE = 12000
+    train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
+                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                            batch_size_fn=batch_size_fn, train=True)
+    valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
+                            repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                            batch_size_fn=batch_size_fn, train=False)
+    model_par = nn.DataParallel(model, device_ids=devices)
 
-for epoch in range(10):
-    model.train()
-    run_epoch(data_gen(V, 30, 20), model,
-              SimpleLossCompute(model.generator, criterion, model_opt))
-    model.eval()
-    print(run_epoch(data_gen(V, 30, 5), model,
-                    SimpleLossCompute(model.generator, criterion, None)))
+if False:
+    model_opt = NoamOpt(model.src_embed[0].d_model, 1, 2000,
+                        torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+    for epoch in range(10):
+        model_par.train()
+        run_epoch((rebatch(pad_idx, b) for b in train_iter),
+                  model_par,
+                  MultiGPULossCompute(model.generator, criterion,
+                                      devices=devices, opt=model_opt))
+        model_par.eval()
+        loss = run_epoch((rebatch(pad_idx, b) for b in valid_iter),
+                         model_par,
+                         MultiGPULossCompute(model.generator, criterion,
+                                             devices=devices, opt=None))
+        print(loss)
+else:
+    model = torch.load("iwslt.pt")
 
+for i, batch in enumerate(valid_iter):
+    src = batch.src.transpose(0, 1)[:1]
+    src_mask = (src != SRC.vocab.stoi["<blank>"]).unsqueeze(-2)
+    out = greedy_decode(model, src, src_mask,
+                        max_len=60, start_symbol=TGT.vocab.stoi["<s>"])
+    print("Translation:", end="\t")
+    for i in range(1, out.size(1)):
+        sym = TGT.vocab.itos[out[0, i]]
+        if sym == "</s>": break
+        print(sym, end=" ")
+    print()
+    print("Target:", end="\t")
+    for i in range(1, batch.trg.size(0)):
+        sym = TGT.vocab.itos[batch.trg.data[i, 0]]
+        if sym == "</s>": break
+        print(sym, end=" ")
+    print()
+    break
 
-def greedy_decode(model, src, src_mask, max_len, start_symbol):
-    memory = model.encode(src, src_mask)
-    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
-    for i in range(max_len - 1):
-        out = model.decode(memory, src_mask,
-                           Variable(ys),
-                           Variable(subsequent_mask(ys.size(1))
-                                    .type_as(src.data)))
-        prob = model.generator(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        next_word = next_word.item()  # was .data[0]
-        ys = torch.cat([ys,
-                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
-    return ys
-
+model, SRC, TGT = torch.load("en-de-model.pt")
 
 model.eval()
-src = Variable(torch.LongTensor([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]))
-src_mask = Variable(torch.ones(1, 1, 10))
-print(greedy_decode(model, src, src_mask, max_len=10, start_symbol=1))
+sent = "▁The ▁log ▁file ▁can ▁be ▁sent ▁secret ly ▁with ▁email ▁or ▁FTP ▁to ▁a ▁specified ▁receiver".split()
+src = torch.LongTensor([[SRC.stoi[w] for w in sent]])
+src = Variable(src)
+src_mask = (src != SRC.stoi["<blank>"]).unsqueeze(-2)
+out = greedy_decode(model, src, src_mask,
+                    max_len=60, start_symbol=TGT.stoi["<s>"])
+print("Translation:", end="\t")
+trans = "<s> "
+for i in range(1, out.size(1)):
+    sym = TGT.itos[out[0, i]]
+    if sym == "</s>": break
+    trans += sym + " "
+print(trans)
+
+tgt_sent = trans.split()
+
+
+def draw(data, x, y, ax):
+    seaborn.heatmap(data,
+                    xticklabels=x, square=True, yticklabels=y, vmin=0.0, vmax=1.0,
+                    cbar=False, ax=ax)
+
+
+for layer in range(1, 6, 2):
+    fig, axs = plt.subplots(1, 4, figsize=(20, 10))
+    print("Encoder Layer", layer + 1)
+    for h in range(4):
+        draw(model.encoder.layers[layer].self_attn.attn[0, h].data,
+             sent, sent if h == 0 else [], ax=axs[h])
+    plt.show()
+
+for layer in range(1, 6, 2):
+    fig, axs = plt.subplots(1, 4, figsize=(20, 10))
+    print("Decoder Self Layer", layer + 1)
+    for h in range(4):
+        draw(model.decoder.layers[layer].self_attn.attn[0, h].data[:len(tgt_sent), :len(tgt_sent)],
+             tgt_sent, tgt_sent if h == 0 else [], ax=axs[h])
+    plt.show()
+    print("Decoder Src Layer", layer + 1)
+    fig, axs = plt.subplots(1, 4, figsize=(20, 10))
+    for h in range(4):
+        draw(model.decoder.layers[layer].self_attn.attn[0, h].data[:len(tgt_sent), :len(sent)],
+             sent, tgt_sent if h == 0 else [], ax=axs[h])
+    plt.show()
