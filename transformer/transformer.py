e2610a87f53da40f816092153bb2fbd144201459
Zongyu <alexyin1012@gmail.com>
2019-02-06 14:06:39 +0000

    Update.
    Signed-off-by: Zongyu <alexyin1012@gmail.com>

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -358,105 +358,105 @@ def data_gen(V, batch, nbatches):
         yield Batch(src, tgt, src_mask, tgt_mask, (tgt[1:] != 0).data.sum())
 
 
-V = 11
-criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
-model = make_model(V, V, N=2)
-model_opt = get_std_opt(model)
-for epoch in range(2):
-    train_epoch(data_gen(V, 30, 20), model, criterion, model_opt)
-#
-# spacy_de = spacy.load('de')
-# spacy_en = spacy.load('en')
-#
-#
-# def tokenize_de(text):
-#     return [tok.text for tok in spacy_de.tokenizer(text)]
-#
-#
-# def tokenize_en(text):
-#     return [tok.text for tok in spacy_en.tokenizer(text)]
-#
-#
-# BOS_WORD = '<s>'
-# EOS_WORD = '</s>'
-# BLANK_WORD = "<blank>"
-# SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
-# TGT = data.Field(tokenize=tokenize_en, init_token=BOS_WORD,
-#                  eos_token=EOS_WORD, pad_token=BLANK_WORD)
-#
-# MAX_LEN = 100
-# train, val, test = datasets.IWSLT.splits(exts=('.de', '.en'), fields=(SRC, TGT),
-#                                          filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
-#                                                                len(vars(x)['trg']) <= MAX_LEN)
-# MIN_FREQ = 1
-# SRC.build_vocab(train.src, min_freq=MIN_FREQ)
-# TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
-#
-# # Detail. Batching seems to matter quite a bit.
-# # This is temporary code for dynamic batching based on number of tokens.
-# # This code should all go away once things get merged in this library.
-#
-# BATCH_SIZE = 4096
-# global max_src_in_batch, max_tgt_in_batch
-#
-#
-# def batch_size_fn(new, count, sofar):
-#     "Keep augmenting batch and calculate total number of tokens + padding."
-#     global max_src_in_batch, max_tgt_in_batch
-#     if count == 1:
-#         max_src_in_batch = 0
-#         max_tgt_in_batch = 0
-#     max_src_in_batch = max(max_src_in_batch, len(new.src))
-#     max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
-#     src_elements = count * max_src_in_batch
-#     tgt_elements = count * max_tgt_in_batch
-#     return max(src_elements, tgt_elements)
-#
-#
-# class MyIterator(data.Iterator):
-#     def create_batches(self):
-#         if self.train:
-#             def pool(d, random_shuffler):
-#                 for p in data.batch(d, self.batch_size * 100):
-#                     p_batch = data.batch(
-#                         sorted(p, key=self.sort_key),
-#                         self.batch_size, self.batch_size_fn)
-#                     for b in random_shuffler(list(p_batch)):
-#                         yield b
-#
-#             self.batches = pool(self.data(), self.random_shuffler)
-#
-#         else:
-#             self.batches = []
-#             for b in data.batch(self.data(), self.batch_size,
-#                                 self.batch_size_fn):
-#                 self.batches.append(sorted(b, key=self.sort_key))
-#
-#
-# def rebatch(pad_idx, batch):
-#     "Fix order in torchtext to match ours"
-#     src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
-#     src_mask, trg_mask = make_std_mask(src, trg, pad_idx)
-#     return Batch(src, trg, src_mask, trg_mask, (trg[1:] != pad_idx).data.sum())
-#
-#
-# train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
-#                         repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-#                         batch_size_fn=batch_size_fn, train=True)
-# valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
-#                         repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
-#                         batch_size_fn=batch_size_fn, train=False)
-#
-# pad_idx = TGT.vocab.stoi["<blank>"]
-# model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
+# V = 11
+# criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
+# model = make_model(V, V, N=2)
 # model_opt = get_std_opt(model)
-# model.cuda()
-#
-# criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
-# criterion.cuda()
-# for epoch in range(15):
-#     train_epoch((rebatch(pad_idx, b) for b in train_iter), model, criterion, model_opt)
-#     valid_epoch((rebatch(pad_idx, b) for b in valid_iter), model, criterion)
+# for epoch in range(2):
+#     train_epoch(data_gen(V, 30, 20), model, criterion, model_opt)
+
+spacy_de = spacy.load('de')
+spacy_en = spacy.load('en')
+
+
+def tokenize_de(text):
+    return [tok.text for tok in spacy_de.tokenizer(text)]
+
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+BOS_WORD = '<s>'
+EOS_WORD = '</s>'
+BLANK_WORD = "<blank>"
+SRC = data.Field(tokenize=tokenize_de, pad_token=BLANK_WORD)
+TGT = data.Field(tokenize=tokenize_en, init_token=BOS_WORD,
+                 eos_token=EOS_WORD, pad_token=BLANK_WORD)
+
+MAX_LEN = 100
+train, val, test = datasets.IWSLT.splits(exts=('.de', '.en'), fields=(SRC, TGT),
+                                         filter_pred=lambda x: len(vars(x)['src']) <= MAX_LEN and
+                                                               len(vars(x)['trg']) <= MAX_LEN)
+MIN_FREQ = 1
+SRC.build_vocab(train.src, min_freq=MIN_FREQ)
+TGT.build_vocab(train.trg, min_freq=MIN_FREQ)
+
+# Detail. Batching seems to matter quite a bit.
+# This is temporary code for dynamic batching based on number of tokens.
+# This code should all go away once things get merged in this library.
+
+BATCH_SIZE = 4096
+global max_src_in_batch, max_tgt_in_batch
+
+
+def batch_size_fn(new, count, sofar):
+    "Keep augmenting batch and calculate total number of tokens + padding."
+    global max_src_in_batch, max_tgt_in_batch
+    if count == 1:
+        max_src_in_batch = 0
+        max_tgt_in_batch = 0
+    max_src_in_batch = max(max_src_in_batch, len(new.src))
+    max_tgt_in_batch = max(max_tgt_in_batch, len(new.trg) + 2)
+    src_elements = count * max_src_in_batch
+    tgt_elements = count * max_tgt_in_batch
+    return max(src_elements, tgt_elements)
+
+
+class MyIterator(data.Iterator):
+    def create_batches(self):
+        if self.train:
+            def pool(d, random_shuffler):
+                for p in data.batch(d, self.batch_size * 100):
+                    p_batch = data.batch(
+                        sorted(p, key=self.sort_key),
+                        self.batch_size, self.batch_size_fn)
+                    for b in random_shuffler(list(p_batch)):
+                        yield b
+
+            self.batches = pool(self.data(), self.random_shuffler)
+
+        else:
+            self.batches = []
+            for b in data.batch(self.data(), self.batch_size,
+                                self.batch_size_fn):
+                self.batches.append(sorted(b, key=self.sort_key))
+
+
+def rebatch(pad_idx, batch):
+    "Fix order in torchtext to match ours"
+    src, trg = batch.src.transpose(0, 1), batch.trg.transpose(0, 1)
+    src_mask, trg_mask = make_std_mask(src, trg, pad_idx)
+    return Batch(src, trg, src_mask, trg_mask, (trg[1:] != pad_idx).data.sum())
+
+
+train_iter = MyIterator(train, batch_size=BATCH_SIZE, device=0,
+                        repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                        batch_size_fn=batch_size_fn, train=True)
+valid_iter = MyIterator(val, batch_size=BATCH_SIZE, device=0,
+                        repeat=False, sort_key=lambda x: (len(x.src), len(x.trg)),
+                        batch_size_fn=batch_size_fn, train=False)
+
+pad_idx = TGT.vocab.stoi["<blank>"]
+model = make_model(len(SRC.vocab), len(TGT.vocab), N=6)
+model_opt = get_std_opt(model)
+model.cuda()
+
+criterion = LabelSmoothing(size=len(TGT.vocab), padding_idx=pad_idx, smoothing=0.1)
+criterion.cuda()
+for epoch in range(15):
+    train_epoch((rebatch(pad_idx, b) for b in train_iter), model, criterion, model_opt)
+    valid_epoch((rebatch(pad_idx, b) for b in valid_iter), model, criterion)
 
 
 
